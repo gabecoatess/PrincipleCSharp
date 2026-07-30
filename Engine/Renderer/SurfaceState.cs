008069b09e4048057c655b940edb890f4227915e
Gabe <54961758+gabecoatess@users.noreply.github.com>
2026-07-30 14:57:22 -0400

    Start from scratch

--- Engine/Renderer/SurfaceState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Principle.API.Renderer;
-
-public enum SurfaceState
-{
-    Normal = 0,
-    Minimized = 1,
-    Maximized = 2,
-    Fullscreen = 3
-}
--- Engine/Runtime/IApplication.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Principle.API.Renderer;
-
-public interface IApplication
-{
-    void Initialize();
-    void Run();
-    void BuildSurface(RenderSurfaceOptions options);
-}
