896cfac030e4ac37d413d6f52dc83ec447877c86
Gabe <54961758+gabecoatess@users.noreply.github.com>
2026-07-30 17:52:06 -0400

    Add new Avalonia editor project

---
+++ Principle.Editor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Principle.Editor.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Principle.Editor/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Principle.Editor.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
