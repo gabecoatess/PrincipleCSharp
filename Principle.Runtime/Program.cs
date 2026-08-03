c5eb640ccf51b2eb25cc02ff19171bb82e9ddab7
Gabe <54961758+gabecoatess@users.noreply.github.com>
2026-08-03 14:20:16 -0400

    Add runtime project instead of sandbox project

---
+++ Principle.Runtime/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Principle.Runtime;
+
+public static class Program
+{
+    private static bool OpenWindow = true;
+
+    public static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            ParseArgs(args);
+        }
+
+        if (OpenWindow)
+        {
+            throw new NotImplementedException("Window mode is not yet implemented!");
+        }
+
+        Console.WriteLine("Hello, Principle!");
+    }
+
+    private static void ParseArgs(string[] args)
+    {
+        foreach (string arg in args)
+        {
+            Console.WriteLine($"Used arg: '{arg}'");
+
+            switch (arg)
+            {
+                case "-nw":
+                case "--no-window":
+                    OpenWindow = false;
+                    
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+    }
+}
