2873381f29e2970df93ae417608f348918470d1a
Gabe <54961758+gabecoatess@users.noreply.github.com>
2026-08-24 16:44:27 -0400

    Remove all previous runtime setup

--- Principle.Runtime/Program.cs
+++ Principle.Runtime/Program.cs
@@ -1,32 +1,15 @@
 using System.Diagnostics;
-using Principle.Engine;
-using TestGameProject;
 
 namespace Principle.Runtime;
 
 public static class Program
 {
-    private static bool _openWindow = true;
-
     public static void Main(string[] args)
     {
         if (args.Length > 0)
         {
             ParseArgs(args);
         }
-
-        var host = new EngineHost();
-
-        var engineHostThread = new Thread(() => host.Run(new MyGame()));
-        engineHostThread.Start();
-
-        if (_openWindow)
-        {
-            throw new NotImplementedException();
-        }
-
-        host.RequestShutdown();
-        engineHostThread.Join();
     }
 
     private static void ParseArgs(string[] args)
@@ -34,12 +17,6 @@ public static class Program
         foreach (var arg in args)
         {
             Debug.WriteLine($"[Runtime] Used arg: '{arg}'");
-
-            _openWindow = arg switch
-            {
-                "-nw" or "--no-window" => false,
-                _ => _openWindow
-            };
         }
     }
 }
